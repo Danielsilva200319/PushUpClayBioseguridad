829d7faf0eaa062e13657e62601365dcc135f9f0
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-12-11 12:28:06 -0500

    Flixing at UnitOfWork

---
+++ Application/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Repositories;
+using Domain.Interfaces;
+using Persistence.Data;
+
+namespace Application.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly BiosegurityContext _context;
+        public UnitOfWork(BiosegurityContext context)
+        {
+            _context = context;
+        }
+        private AddressTypeRepository _addresstypes;
+        private CityRepository _cities;
+        private ContactTypeRepository _contacttypes;
+        private ContractRepository _contracts;
+        private DepartmentRepository _departments;
+        private PersonRepository _persons;
+        private PersonAddressRepository _personAddresses;
+        private PersonCategoryRepository _personCategories;
+        private PersonContactRepository _personContacts;
+        private PersonTypeRepository _personTypes;
+        private ProgrammingRepository _programmings;
+        private ShiftRepository _shifts;
+        private StateRepository _states;
+
+
+        public IAddressType AddressTypes
+        {
+            get
+            {
+                if (_addresstypes == null)
+                {
+                    _addresstypes = new AddressTypeRepository(_context);
+                }
+                return _addresstypes;
+            }
+        }
+
+        public ICity Cities
+        {
+            get
+            {
+                if (_cities == null)
+                {
+                    _cities = new CityRepository(_context);
+                }
+                return _cities;
+            }
+        }
+
+        public IContactType ContactTypes
+        {
+            get
+            {
+                if (_contacttypes == null)
+                {
+                    _contacttypes = new ContactTypeRepository(_context);
+                }
+                return _contacttypes;
+            }
+        }
+
+        public IContract Contracts
+        {
+            get
+            {
+                if (_contracts == null)
+                {
+                    _contracts = new ContractRepository(_context);
+                }
+                return _contracts;
+            }
+        }
+
+        public IDepartment Departments
+        {
+            get
+            {
+                if (_departments == null)
+                {
+                    _departments = new DepartmentRepository(_context);
+                }
+                return _departments;
+            }
+        }
+
+        public IPerson Persons
+        {
+            get
+            {
+                if (_persons == null)
+                {
+                    _persons = new PersonRepository(_context);
+                }
+                return _persons;
+            }
+        }
+
+        public IPersonAddress PersonAddresses
+        {
+            get
+            {
+                if (_personAddresses == null)
+                {
+                    _personAddresses = new PersonAddressRepository(_context);
+                }
+                return _personAddresses;
+            }
+        }
+
+        public IPersonCategory PersonCategories
+        {
+            get
+            {
+                if (_personCategories == null)
+                {
+                    _personCategories = new PersonCategoryRepository(_context);
+                }
+                return _personCategories;
+            }
+        }
+
+        public IPersonContact PersonContacts
+        {
+            get
+            {
+                if (_personContacts == null)
+                {
+                    _personContacts = new PersonContactRepository(_context);
+                }
+                return _personContacts;
+            }
+        }
+
+        public IPersonType PersonTypes
+        {
+            get
+            {
+                if (_personTypes == null)
+                {
+                    _personTypes = new PersonTypeRepository(_context);
+                }
+                return _personTypes;
+            }
+        }
+
+        public IProgramming Programmings
+        {
+            get
+            {
+                if (_programmings == null)
+                {
+                    _programmings = new ProgrammingRepository(_context);
+                }
+                return _programmings;
+            }
+        }
+
+        public IShift Shifts
+        {
+            get
+            {
+                if (_shifts == null)
+                {
+                    _shifts = new ShiftRepository(_context);
+                }
+                return _shifts;
+            }
+        }
+
+        public IState States
+        {
+            get
+            {
+                if (_states == null)
+                {
+                    _states = new StateRepository(_context);
+                }
+                return _states;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
--- Persistence/Data/BiosegurityContext.cs
+++ Persistence/Data/BiosegurityContext.cs
@@ -44,6 +44,10 @@ public partial class BiosegurityContext : DbContext
     public virtual DbSet<Shift> Shifts { get; set; }
 
     public virtual DbSet<State> States { get; set; }
+    public virtual DbSet<Rol> Rols { get; set; }
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
+    public virtual DbSet<UserRol> UserRols { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
